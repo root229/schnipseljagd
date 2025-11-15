f94d1cb2f6979fc4cec1eceb38ed34a6e2da77a7
JohnnyBoyXY <122137338+root229@users.noreply.github.com>
2025-11-15 23:46:05 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+function lösungswortChecken() {
+        const lösungswort = prompt("Was ist dein Lösungswort?");
+        if (lösungswort && lösungswort.toLowerCase() === "meinung") {
+            window.open("https://forms.gle/bzeTBfoebryMJDmu7", "_blank");
+        }
+        else if (lösungswort.toLowerCase() === "start") {
+            window.open("https://docs.google.com/forms/d/e/1FAIpQLSfjG_xzTMmzNkgtNZpIAD2OVkhEJDk9W6CGO-RXNXp1qVeevg/viewform?usp=header", "_blank");
+        }
+        else if (lösungswort.toLowerCase() === "usikra") {
+            window.open("https://forms.gle/yroMJwJoj5cTbTXw9", "_blank")
+        }
+    }
