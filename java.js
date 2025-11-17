fb1fedc4759ea89acfbbea0de9e8d6550221e380
JohnnyBoyXY <122137338+root229@users.noreply.github.com>
2025-11-17 08:03:17 +0100

    java.js aktualisieren

--- java.js
+++ java.js
@@ -3,13 +3,13 @@ function lösungswortChecken() {
         if (lösungswort && lösungswort.toLowerCase() === "meinung") {
             window.open("https://forms.gle/bzeTBfoebryMJDmu7", "_blank");
         }
-        else if (lösungswort.toLowerCase() === "start") {
+        if (lösungswort.toLowerCase() === "start") {
             window.open("https://docs.google.com/forms/d/e/1FAIpQLSfjG_xzTMmzNkgtNZpIAD2OVkhEJDk9W6CGO-RXNXp1qVeevg/viewform?usp=header", "_blank");
         }
-        else if (lösungswort.toLowerCase() === "usikra") {
+        if (lösungswort.toLowerCase() === "usikra") {
             window.open("https://forms.gle/yroMJwJoj5cTbTXw9", "_blank")
         }
-        else if (lösungswort.toLowerCase() === "musikraum") {
-            window.open("https://root229.github.io/Info_und_Anleitung/", "_blank")
+        if (lösungswort.toLowerCase() === "musikraum") {
+            window.open("https://root229.github.io/info_und_anleitung/", "_blank")
         }
     }
